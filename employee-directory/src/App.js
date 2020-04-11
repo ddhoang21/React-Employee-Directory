2dae6985c13d9fb7c6295a5603bd6c4a6159df96
DDH <ddhoang1993@gmail.com>
2020-04-11 09:59:57 -0700

    added header and wrapper

---
+++ employee-directory/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
